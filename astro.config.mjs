bb26048e157085075a09ec2912cf74ef3ef0b3b1
Olivier Gendrin <adrien.durot@ext.bpifrance.fr>
2026-02-19 10:10:40 +0100

    Add a preact greetings component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://lighthearted-crumble-1fbbf2.netlify.app/"
+  site: "https://lighthearted-crumble-1fbbf2.netlify.app/",
+  integrations: [preact()]
 });
