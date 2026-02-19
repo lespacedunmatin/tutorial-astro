55d538178b67f5a7c50be1a722281ddf540b7de2
Olivier Gendrin <adrien.durot@ext.bpifrance.fr>
2026-02-19 09:36:13 +0100

    Add RSS feed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://lighthearted-crumble-1fbbf2.netlify.app/"
+});
